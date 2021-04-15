44bd5962794a1cb97c482ef4265c86174f287637
Visan <visan.costin47@gmail.com>
2021-04-15 11:15:46 +0300

    B.1

---
+++ PracticalTest01Var05/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var05/general/Constants.java
@@ -0,0 +1,9 @@
+package ro.pub.cs.systems.eim.practicaltest01var05.general;
+
+public class Constants {
+    final public static String TOP_LEFT = "Top Left";
+    final public static String TOP_RIGHT = "Top Right";
+    final public static String BOTTOM_LEFT = "Bottom Left";
+    final public static String BOTTOM_RIGHT = "Bottom Right";
+    final public static String CENTER = "Center";
+}
--- PracticalTest01Var05/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var05/view/PracticalTest01Var05MainActivity.java
+++ PracticalTest01Var05/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var05/view/PracticalTest01Var05MainActivity.java
@@ -2,14 +2,43 @@ package ro.pub.cs.systems.eim.practicaltest01var05.view;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 import ro.pub.cs.systems.eim.practicaltest01var05.R;
+import ro.pub.cs.systems.eim.practicaltest01var05.general.Constants;
 
 public class PracticalTest01Var05MainActivity extends Activity {
-    Button topLeftButton, topRightButton, navigateToSecondaryActivityButton, bottomLeftButton, bottomRighButton;
+    Button topLeftButton, topRightButton, navigateToSecondaryActivityButton, bottomLeftButton, bottomRighButton, centerButton;
+    TextView resutView;
+    private ButtonClickListener buttonClickListener = new ButtonClickListener();
+    private class ButtonClickListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.top_left_button:
+                    resutView.setText(resutView.getText().toString() + ", " + Constants.TOP_LEFT);
+                    break;
+                case R.id.top_right_button:
+                    resutView.setText(resutView.getText().toString() + ", " + Constants.TOP_RIGHT);
+                    break;
+                case R.id.bottom_right_button:
+                    resutView.setText(resutView.getText().toString() + ", " + Constants.BOTTOM_RIGHT);
+                    break;
+                case R.id.bottom_left_button:
+                    resutView.setText(resutView.getText().toString() + ", " + Constants.BOTTOM_LEFT);
+                    break;
+                case R.id.center_button:
+                    resutView.setText(resutView.getText().toString() +  ", " + Constants.CENTER);
+                    break;
+            }
+        }
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +47,14 @@ public class PracticalTest01Var05MainActivity extends Activity {
         topRightButton = findViewById(R.id.top_right_button);
         bottomLeftButton = findViewById(R.id.bottom_left_button);
         bottomRighButton = findViewById(R.id.bottom_right_button);
+        centerButton = findViewById(R.id.center_button);
         navigateToSecondaryActivityButton = findViewById(R.id.navigate_to_secondary_activity_button);
+        resutView = findViewById(R.id.result_view);
 
+        topRightButton.setOnClickListener(buttonClickListener);
+        topLeftButton.setOnClickListener(buttonClickListener);
+        bottomRighButton.setOnClickListener(buttonClickListener);
+        bottomLeftButton.setOnClickListener(buttonClickListener);
+        centerButton.setOnClickListener(buttonClickListener);
     }
 }
