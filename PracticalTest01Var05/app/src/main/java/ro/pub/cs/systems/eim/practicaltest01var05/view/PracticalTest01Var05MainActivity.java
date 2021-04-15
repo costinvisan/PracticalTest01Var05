43a63af83a9560ff9bdff5ef756f3b19d1e1b7cc
Visan <visan.costin47@gmail.com>
2021-04-15 11:03:36 +0300

    A.2

--- PracticalTest01Var05/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var05/view/PracticalTest01Var05MainActivity.java
+++ PracticalTest01Var05/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var05/view/PracticalTest01Var05MainActivity.java
@@ -2,12 +2,23 @@ package ro.pub.cs.systems.eim.practicaltest01var05.view;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 
+import ro.pub.cs.systems.eim.practicaltest01var05.R;
+
 public class PracticalTest01Var05MainActivity extends Activity {
+    Button topLeftButton, topRightButton, navigateToSecondaryActivityButton, bottomLeftButton, bottomRighButton;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test01_var05_main);
+        topLeftButton = findViewById(R.id.top_left_button);
+        topRightButton = findViewById(R.id.top_right_button);
+        bottomLeftButton = findViewById(R.id.bottom_left_button);
+        bottomRighButton = findViewById(R.id.bottom_right_button);
+        navigateToSecondaryActivityButton = findViewById(R.id.navigate_to_secondary_activity_button);
+
     }
 }
